1fd85f2df746161500ad91458178ffc008eadb94
stemaweb <noreply>
2015-06-27 10:14:45 +0000

    Add component stage

---
+++ ui/main.reel/stage.reel/stage.js
@@ -0,0 +1,16 @@
+/**
+ * @module ui/main.reel/stage.reel
+ */
+var Component = require("montage/ui/component").Component;
+
+/**
+ * @class Stage
+ * @extends Component
+ */
+exports.Stage = Component.specialize(/** @lends Stage# */ {
+    constructor: {
+        value: function Stage() {
+            this.super();
+        }
+    }
+});
